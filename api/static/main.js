659dbb67c200a1efcc46d16cb42f6f61bd3f8f43
kyamuise <kyamuise@gmail.com>
2017-10-29 00:22:00 +0900

    cirtifiction

--- api/static/main.js
+++ api/static/main.js
@@ -21,12 +21,11 @@ recognition.addEventListener('result', function(event){
     if(text.length > 10) {
         stop();
     }
-    $.get("http://kyamuise.xyz:5000/incr/1");
 
     if(event.results.item(0).isFinal) {
         if(/だめ|ダメ|でも|しかし|いや|嫌/.test(text)) {
             console.log("negative");
-            $.get("http://kyamuise.xyz:5000/incr/1");
+            $.get("https://kyamuise.xyz:5000/incr/1");
         }
         console.log(event.results);
         setTimeout(record, 50);
