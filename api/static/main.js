8303290954a0bb503a5ea7776ac390d33f68b6a7
kyamuise <kyamuise@gmail.com>
2017-10-29 03:16:04 +0900

    https

--- api/static/main.js
+++ api/static/main.js
@@ -5,7 +5,8 @@ recognition.lang = 'ja';
 recognition.continuous = true;
 recognition.interimResults = true;
 
-var negativeWords = /だめ|ダメ|でも|しかし|いや|嫌/;
+var negativeWords = /だめ|ダメ|でも|しかし|いや|嫌|良くない|最悪|悪い/;
+var positiveWords = /おっけー|OK|はい|了解|なるほど|いいね/;
 
 function record() {
     recognition.start();
@@ -27,7 +28,7 @@ recognition.addEventListener('result', function(event){
     if(event.results.item(0).isFinal) {
         if(negativeWords.test(text)) {
             console.log("negative");
-            $.get("http://kyamuise.xyz:1242/incr/1");
+            $.get("https://kyamuise.xyz:1242/incr/1");
         }
         console.log(event.results);
         setTimeout(record, 50);
